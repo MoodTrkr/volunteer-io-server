7dcd90a52759efeda88c00b234995e3b640bec71
itsfeas <amf.fiaz@gmail.com>
2022-07-08 10:41:08 -0600

    enabling auth0 + fixed routes issue

--- api/src/controllers/usageData/usageDataController.ts
+++ api/src/controllers/usageData/usageDataController.ts
@@ -19,13 +19,15 @@ require('dotenv').config({ path: './src/auth/secret-key.env' });
 const insertUsageData = controller((req: ExpressExtended.AuthenticatedRequest, res: Response) => {
     const params = req.body;
     console.log('insert begin');
-    wrappers.basic(() => {
-        const user = req.user;
-        console.log(user);
-        console.log(req.body);
-        // const usageData = req.body as mdtkrSchema.MTData.MTUsageData;
-        // if (isUserDefined(user)) { usageRepo.insertUsageData(user, usageData.date, usageData) }
-    })
+    const user = req.user;
+    console.log(user);
+    console.log(req.body);
+    res.status(200).send(200);
+    // wrappers.basic(() => {
+
+    //     // const usageData = req.body as mdtkrSchema.MTData.MTUsageData;
+    //     // if (isUserDefined(user)) { usageRepo.insertUsageData(user, usageData.date, usageData) }
+    // })
 });
 
 const getUsageData = async (req: ExpressExtended.AuthenticatedRequest, res: Response) => {
