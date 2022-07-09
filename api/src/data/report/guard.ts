b364075437fddbee05d91b234e615422d38e63b1
itsfeas <amf.fiaz@gmail.com>
2022-07-09 16:31:02 -0600

    created type guard for usagedata obj

---
+++ api/src/data/report/guard.ts
@@ -0,0 +1,20 @@
+import { Request, Response, NextFunction } from "express";
+import type * as mdtkrSchema from '../../data/report/schema';
+
+export default function isMTUsageData(body: any | undefined): body is mdtkrSchema.MTData.MTUsageData {
+    if (body.dailyCollection) {
+        const dailyColl = body.dailyCollection;
+        if (dailyColl.callLogs && dailyColl.date && dailyColl.screenTime
+            && dailyColl.sleepData && dailyColl.usageLogs && dailyColl.usageStats) null
+        else {
+            return false
+        }
+    }
+    else {
+        return false
+    }
+    if (body.date) { null } else { return false }
+    if (body.periodicCollBook) { null } else { return false }
+    if (body.surveyData) { null } else { return false }
+    return true;
+}
