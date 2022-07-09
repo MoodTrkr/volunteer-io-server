becdab14c5fb79a05b3dd8506c4b71ec2ff8ce82
itsfeas <amf.fiaz@gmail.com>
2022-07-09 15:53:19 -0600

    testing wrapper

--- api/src/controllers/usageData/usageDataController.ts
+++ api/src/controllers/usageData/usageDataController.ts
@@ -19,13 +19,14 @@ require('dotenv').config({ path: './src/auth/secret-key.env' });
 const insertUsageData = controller((req: ExpressExtended.AuthenticatedRequest, res: Response) => {
     const params = req.body;
     console.log('insert begin');
-    wrappers.basic((req: ExpressExtended.AuthenticatedRequest, res: Response) => {
-        console.log("user", req.user);
-        console.log("auth", req.auth);
-        console.log(req.body);
-        // const usageData = req.body as mdtkrSchema.MTData.MTUsageData;
-        // if (isUserDefined(user)) { usageRepo.insertUsageData(user, usageData.date, usageData) }
-    })
+    console.log("user", req.user);
+    console.log("auth", req.auth);
+    console.log(req.body);
+    res.status(200).send(true);
+    // wrappers.basic((req: ExpressExtended.AuthenticatedRequest, res: Response) => {
+    //     // const usageData = req.body as mdtkrSchema.MTData.MTUsageData;
+    //     // if (isUserDefined(user)) { usageRepo.insertUsageData(user, usageData.date, usageData) }
+    // })
 });
 
 const getUsageData = async (req: ExpressExtended.AuthenticatedRequest, res: Response) => {
