4e1cd45e0363598bdb0cc3e82b592a98c3155998
itsfeas <amf.fiaz@gmail.com>
2022-07-09 20:08:23 -0600

    changed api sdks

--- api/src/auth/auth.ts
+++ api/src/auth/auth.ts
@@ -3,17 +3,23 @@ import process from 'process';
 require('dotenv').config({ path: 'auth/secret-key.env' });
 import { expressjwt } from 'express-jwt';
 var jwks = require('jwks-rsa');
+import { auth } from 'express-oauth2-jwt-bearer';
 
-var checkJwt = expressjwt({
-	secret: jwks.expressJwtSecret({
-		cache: true,
-		rateLimit: true,
-		jwksRequestsPerMinute: 5,
-		jwksUri: process.env.AUTH0_JWKS_URI
-	}),
-	audience: process.env.AUTH0_AUDIENCE,
-	issuer: process.env.AUTH0_ISSUER,
-	algorithms: ['RS256']
+// var checkJwt = expressjwt({
+// 	secret: jwks.expressJwtSecret({
+// 		cache: true,
+// 		rateLimit: true,
+// 		jwksRequestsPerMinute: 5,
+// 		jwksUri: process.env.AUTH0_JWKS_URI
+// 	}),
+// 	audience: process.env.AUTH0_AUDIENCE,
+// 	issuer: process.env.AUTH0_ISSUER,
+// 	algorithms: ['RS256']
+// });
+
+const checkJwt = auth({
+	audience: 'YOUR_API_IDENTIFIER',
+	issuerBaseURL: `https://YOUR_DOMAIN/`,
 });
 
 export {
