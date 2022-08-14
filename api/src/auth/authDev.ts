71c64b4dafc2477437c6f57fc053127eceb06ac8
itsfeas <amf.fiaz@gmail.com>
2022-08-14 13:15:18 -0600

    changed auth-dev middleware

--- api/src/auth/authDev.ts
+++ api/src/auth/authDev.ts
@@ -5,9 +5,16 @@ import { expressjwt } from 'express-jwt';
 var jwks = require('jwks-rsa');
 import { auth } from 'express-oauth2-jwt-bearer';
 
-const checkJwtDev = auth({
-	audience: process.env.AUTH0_AUDIENCE_DEV,
-	issuerBaseURL: process.env.AUTH0_ISSUER_DEV
+var checkJwtDev = expressjwt({
+	secret: jwks.expressJwtSecret({
+		cache: true,
+		rateLimit: true,
+		jwksRequestsPerMinute: 5,
+		jwksUri: 'https://dev-dh23pufb.us.auth0.com/.well-known/jwks.json'
+	}),
+	audience: 'https://quickstarts/mdtkr-api-rest',
+	issuer: 'https://dev-dh23pufb.us.auth0.com/',
+	algorithms: ['RS256']
 });
 
 export {
