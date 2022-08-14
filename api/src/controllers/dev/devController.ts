e60e933e1b799f849a8d47ffc523e8f7a8f9c9ca
itsfeas <amf.fiaz@gmail.com>
2022-08-14 16:21:05 -0600

    now made this synchronous

--- api/src/controllers/dev/devController.ts
+++ api/src/controllers/dev/devController.ts
@@ -17,7 +17,7 @@ const getAllUsageData = async (req: ExpressExtended.AuthenticatedRequest, res: R
     data.forEach(entry => {
         dataDecompressed.push(
             zlib.brotliDecompressSync(
-                Buffer.from(entry?.usage_data), zlib.constants.BROTLI_OPERATION_FLUSH
+                Buffer.from(entry?.usage_data), { finishFlush: zlib.constants.BROTLI_OPERATION_FLUSH }
             ));
     })
     if (data.length>0) return res.status(200).json(dataDecompressed);
