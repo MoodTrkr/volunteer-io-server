e4e56504f23edff14f67490f9e2bdc944905fe0b
itsfeas <amf.fiaz@gmail.com>
2022-08-17 20:43:56 -0600

    testing

--- api/src/controllers/dev/devController.ts
+++ api/src/controllers/dev/devController.ts
@@ -13,20 +13,21 @@ const encrypt = async (data: String) => {
 
 const getAllUsageData = async (req: ExpressExtended.AuthenticatedRequest, res: Response) => {
     var data = await usageRepo.getAllUsageDataDev();
-    var dataFormatted = Array();
-    data.forEach(entry => {
-        var entryFormatted = Object()
-        entryFormatted.id = entry.id;
-        entryFormatted.id_user = entry.id_user;
-        entryFormatted.ts = entry.ts;
-        entryFormatted.usage_data = zlib.brotliDecompressSync(
-            Buffer.from(entry.usage_data, 'base64'),
-            { finishFlush: zlib.constants.BROTLI_OPERATION_FLUSH }
-        ).toString("utf8");
-        console.log(entryFormatted.usage_data);
-        dataFormatted.push(entryFormatted);
-    })
-    if (data.length>0) return res.status(200).json(dataFormatted);
+    // var dataFormatted = Array();
+    // data.forEach(entry => {
+    //     var entryFormatted = Object()
+    //     entryFormatted.id = entry.id;
+    //     entryFormatted.id_user = entry.id_user;
+    //     entryFormatted.ts = entry.ts;
+    //     entryFormatted.usage_data = zlib.brotliDecompressSync(
+    //         Buffer.from(entry.usage_data, 'base64'),
+    //         { finishFlush: zlib.constants.BROTLI_OPERATION_FLUSH }
+    //     ).toString("utf8");
+    //     console.log(entryFormatted.usage_data);
+    //     dataFormatted.push(entryFormatted);
+    // })
+    // if (data.length>0) return res.status(200).json(dataFormatted);
+    if (data.length > 0) return res.status(200).json(data);
     else return res.status(200);
 };
 
